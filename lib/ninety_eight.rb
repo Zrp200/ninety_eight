9d20772f51b74ea9f0470cda7a75e731bb048c13
Zachary Perlmutter <zrp200@gmail.com>
2014-06-29 10:32:36 -0700

    added documentation

--- lib/ninety_eight.rb
+++ lib/ninety_eight.rb
@@ -1,8 +1,11 @@
+# Errors that happen in the game. If you get this error, everything is working fine.
 class CardError < StandardError; end
-class Card
+class Card # The objects that are used in gameplay. Found in _$deck_.
+# The number on the card
 	attr_reader :num
+# Creates a new card.
 	def initialize(card); @num = card; end
-	def value
+	def value # Returns the Card's value based on its :num attribute.
 		case self.num
 			when "Ace" then return 1
 			when 2..9 then return self.num
@@ -12,22 +15,26 @@ class Card
 		end
 	end
 end
+# A subclass of Card that can imitate its superclass. Used to make user input Card-like.
 class UserCard < Card
+# Abbrevations
 	@@num = {King: "King", K: "King", Queen: "Queen", Q: "Queen", Ace: "Ace", A: "Ace", Jack: "Jack", J: "Jack"}
-	def initialize(card)
+	def initialize(card) # Creates a new UserCard. Looks at abbrevations and modifies user input so it is Card-like.
 		@@num.default = card.to_i
 		@num = @@num[card.capitalize.to_sym]
 	end
 end
-class Hand
+class Hand # The gameplay happens here. Holds four cards and interacts with _$deck_.
+# The player's actual hand
 	attr_reader :hand
-	def initialize
+	def initialize # Creates a new Hand. Takes four cards from _$deck_ and shuffles _$deck_.
 		$deck.shuffle!
 		@hand = [$deck.shift, $deck.shift, $deck.shift, $deck.shift]
 		$deck.shuffle!
 	end
+# Lists the cards in attribute :hand.
 	def list; @hand.each {|card| print "\t#{card.num}"}; end
-	def play(card)
+	def play(card) # Gameplay method
 		$legal, i, done = false, 0, false
 		for cards in @hand
 			if cards.num == card.num and done == false
@@ -36,10 +43,10 @@ class Hand
 				$deck.shuffle!
 				draw = $deck.shift
 				discard = @hand[i]
-				@hand.delete_at(i)
-				$deck.push(discard)
+				@hand.delete_at i
+				$deck.push discard
 				$deck.shuffle!
-				@hand.push(draw)
+				@hand << draw
 			end
 			i += 1
 		end
