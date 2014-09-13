55fdb261006b27ccffdfceb633eabc8fe9dd21b6
Zachary Perlmutter <zrp200@gmail.com>
2014-09-12 18:37:27 -0700

    Update 98_spec.rb

--- spec/lib/98_spec.rb
+++ spec/lib/98_spec.rb
@@ -2,10 +2,10 @@ describe "test" do
 	context "when $value = 98" do
 		$value = 98 
 		value = nil
-		context "and card = Card.new \"King\"" do
+		context "and card = Card.new 'King'" do
 			subject {test(value, Card.new("King"))}
 			it "should == 98" do
-				expect(test(subject).to eq 98
+				expect(subject).to eq 98
 			end
 		end
 	end
