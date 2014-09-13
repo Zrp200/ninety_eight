d75b1baf52ddf0e6b85dff2dc07ecc5c0dbe2a58
Zachary Perlmutter <zrp200@gmail.com>
2014-09-12 18:38:52 -0700

    Update 98_spec.rb

--- spec/lib/98_spec.rb
+++ spec/lib/98_spec.rb
@@ -1,7 +1,7 @@
 describe "test" do
 	context "when $value = 98" do
 		$value = 98 
-		value = nil
+		value = 0
 		context "and card = Card.new 'King'" do
 			subject {test(value, Card.new("King"))}
 			it "should == 98" do
