f7b22edd0b69860d970a4bc5cd36397bb3f29592
Zachary Perlmutter <zrp200@gmail.com>
2014-09-12 18:32:24 -0700

    Update 98_spec.rb

--- spec/lib/98_spec.rb
+++ spec/lib/98_spec.rb
@@ -1,4 +1,9 @@
-require "spec_helper"
-describe Card do
-	
+describe "test" do
+	context "when $value = 98" do
+		$value = 98 
+		value = nil
+		context "and card = Card.new \"King\"" do
+			it "should == 98" {expect(test(value, Card.new("King"))).to eq 98}
+		end
+	end
 end
