c8c75b18ce550999c7a5baccde2c29157ff29a10
Zachary Perlmutter <zrp200@gmail.com>
2014-09-12 18:42:36 -0700

    Update 98_spec.rb

--- spec/lib/98_spec.rb
+++ spec/lib/98_spec.rb
@@ -1,12 +1,6 @@
-describe "test" do
-	context "when $value = 98" do
-		$value = 98 
-		value = 0
-		context "and card = Card.new 'King'" do
-			subject {test(value, Card.new("King"))}
-			it "should == 98" do
-				expect(subject).to eq 98
-			end
+RSpec.describe Card do
+	describe "#value" do
+		context "when 2-10" do
 		end
 	end
 end
