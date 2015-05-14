e8328ef45bbe133467fb57c88d5c1f96432315ea
Zachary Perlmutter <zrp200@gmail.com>
2015-05-14 12:40:52 -0700

    Update ninety_eight.rb

--- lib/ninety_eight.rb
+++ lib/ninety_eight.rb
@@ -10,37 +10,7 @@ def pause(p) # Shortcut for sleep and a newline.
 	sleep p
 	puts
 end
-class Hand # The gameplay happens here. Holds four cards and interacts with $deck.
-# The player's actual hand
-	attr_reader :hand
-	def initialize # Creates a new Hand. Takes four cards from $deck and shuffles $deck.
-		Deck.shuffle!
-		@hand = $deck.shift, $deck.shift, $deck.shift, $deck.shift
-		Deck.shuffle!
-	end
-# Lists the cards in attribute :hand.
-	def list
-		@hand.each {|card| print "\t#{card.num}"}
-	end
-	def play(card) # Gameplay method
-		$legal, i, done = false, 0, false
-		for cards in @hand
-			if cards.num == card.num and done == false
-				done, $legal = true
-				Deck.shuffle!
-				draw, discard = $deck.shift, @hand[i]
-				@hand.delete_at i
-				Deck.push discard
-				Deck.shuffle!
-				@hand << draw
-			end
-			i += 1
-		end
-		raise CardError, "\aCard not Allowed\a" unless $legal
-		if card.num == "King" then $value = 98
-		else $value += card.value end
-	end
-end
+
 Deck = Array.new
 4.times do
 	Deck.shuffle!
